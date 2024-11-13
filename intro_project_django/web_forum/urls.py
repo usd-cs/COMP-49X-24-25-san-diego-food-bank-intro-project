d0f3165c050f59f9cf02ae4ee3bdbfb44e47ef91
Gianpaolo <gtabora@sandiego.edu>
2024-11-13 13:24:16 -0800

    Implemented create_new_post UI and added button functionality

--- intro_project_django/web_forum/urls.py
+++ intro_project_django/web_forum/urls.py
@@ -2,10 +2,13 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("post/", views.post_view, name="post"),
     path("create_post/", views.create_post_view, name="create post"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
