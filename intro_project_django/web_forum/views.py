bb74accc9578c6986f39b899b793667e62b521e9
Brendan Bost <bbost@sandiego.edu>
2024-11-12 19:48:40 -0800

    trying to add these changes again

--- intro_project_django/web_forum/views.py
+++ intro_project_django/web_forum/views.py
@@ -1,16 +1,32 @@
 """Temp docstring for linting"""
 
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
-def index():
-    """Handle the home/login page."""
-    return HttpResponse("This is the home/login page.")
+def login_view(request):
+    """
+    Properly handle user login by displaying the login form and processing the user authentication.
 
-def posts():
-    """Handle the posts page."""
-    return HttpResponse("This is the posts page.")
+    This view will display the login form for users to input their email and password. It will validate
+    the user's credentials and log them in where they will be redirected to the home page.
+    """
+    if request.method == 'POST': # Make sure the request is 'POST' to ensure user submits form to be processed
+        form = AuthenticationForm(data = request.POST) 
+        if form.is_valid(): 
+            user = form.get_user() # Retrieve the user if form is valid
+            login(request, user) 
+            return redirect('home')
+    return render(request, 'login.html', {'form': form})
 
-def post():
-    """Handle viewing a specific post by its ID."""
-    return HttpResponse("This is a path to see a specific post (postID/post/)")
+def home_view(request):
+    pass # Setup later
+
+def post_view(request):
+    pass # Setup later
+
+def create_post_view(request):
+    pass # Setup later
