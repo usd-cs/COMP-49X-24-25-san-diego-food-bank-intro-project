b43657f703e310de9a7f69209909c4c037a8db49
Evan Scott <evan.scott@dc.rr.com>
2024-11-13 16:19:59 -0800

    Convert sql schema into django models for easier CRUD operations in app

--- intro_project_django/web_forum/models.py
+++ intro_project_django/web_forum/models.py
@@ -1,4 +1,21 @@
-"""Temp docstring for linting"""
-#from django.db import models
+"""Models defining the tables within Postgresql database"""
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    """User table for storing relevant information on admins and users"""
+    email = models.EmailField(unique=True)
+    admin = models.BooleanField(default=False)
+
+class Post(models.Model):
+    """Post table for storing relevant information for posts"""
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts')
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    """Comment table for storign relevant information for comments"""
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='comments')
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
