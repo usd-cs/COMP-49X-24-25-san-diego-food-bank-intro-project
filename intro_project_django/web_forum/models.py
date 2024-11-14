a4987fadeeca37841130c35df885be8e963a5733
Evan Scott <evan.scott@dc.rr.com>
2024-11-13 17:26:37 -0800

    Fix actions linting workflow

--- intro_project_django/web_forum/models.py
+++ intro_project_django/web_forum/models.py
@@ -3,18 +3,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    """User table for storing relevant information on admins and users"""
+    """User table for storing information on admins and users"""
     email = models.EmailField(unique=True)
     admin = models.BooleanField(default=False)
 
 class Post(models.Model):
-    """Post table for storing relevant information for posts"""
+    """Post table for storing information for posts"""
     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts')
     contents = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
-    """Comment table for storign relevant information for comments"""
+    """Comment table for storign information for comments"""
     post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='comments')
     contents = models.TextField()
