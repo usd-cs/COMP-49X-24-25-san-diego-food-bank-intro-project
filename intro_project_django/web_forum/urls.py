0213f9a6029c32e162f4e52f62d271485c54ba4f
Evan Scott <evan.scott@dc.rr.com>
2024-11-14 02:12:23 -0800

    Implement admin functionality for deleting posts from home page

--- intro_project_django/web_forum/urls.py
+++ intro_project_django/web_forum/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("create_post/", views.create_post_view, name="create post"),
     path("login/", views.login_view, name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path('reply/', views.reply_view, name='reply'),
+    path('reply/<int:post_id>/', views.reply_view, name='reply'),
+    path('delete/<int:post_id>/', views.delete_post_view, name="delete")
 ]
